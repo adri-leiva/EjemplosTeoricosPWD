888104565d99887afef4b1fe34f5e3bc1e58858b
adriana.leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2021-03-20 20:13:49 -0300

    Pedimos que ingrese un dato por pantalla

---
+++ Validar datos/ValidacionDeFormularios.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <title>Valida datos</title>
+    <script type="text/javascript" src="Verificar.js"></script>
+</head>
+<body>
+
+    <form name="form1" method="post" action="RecibirDatosPost.php" onsubmit="validar()">
+        Ingrese dato: <input name="dato_form" type="text" id="dato_form" /><br/>
+    <input type="submit" name="submit" value="Aceptar" />
+</form>
+    
+</body>
+</html>
