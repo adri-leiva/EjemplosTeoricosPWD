ef13205a4c88f883a0673300389eb063f44aba63
adriana.leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2021-03-21 16:11:48 -0300

    Agregamos return a onsubmit para detener el envio del formulario si la función validar nos devuelve falso

--- Validar datos/ValidacionDeFormularios.php
+++ Validar datos/ValidacionDeFormularios.php
@@ -6,7 +6,10 @@
 </head>
 <body>
 
-    <form name="form1" method="post" action="RecibirDatosPost.php" onsubmit="validar()">
+    <form name="form1" method="post" action="RecibirDatosPost.php" onsubmit="return validar();">
+    
+    <!-- Uso del return: Si la funcion validar() devuelve false los datos no se envian al servidor. Se detiene el envio del formulario -->
+
         Ingrese dato: <input name="dato_form" type="text" id="dato_form" /><br/>
     <input type="submit" name="submit" value="Aceptar" />
 </form>
